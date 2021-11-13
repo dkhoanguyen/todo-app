b114071b48cb6fe1ece6bad193d947868c3b652c
Khoa Nguyen <khow.nguyen@presien.com>
2021-11-14 09:44:32 +1100

    Updated user models and handlers

--- controllers/handlers.user.go
+++ controllers/handlers.user.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +15,24 @@ func performLogin(c *gin.Context) {
 	var sameSiteCookie http.SameSite
 
 	if models.isUserValid(username, password) {
-
+		// If the username/password is valid set the token in a cookie
+		token := generateSessionToken()
+		c.SetCookie("token", token, 3600, "", "", sameSiteCookie, false, true)
+		c.Set("is_logged_in", true)
+		render(c, gin.H{
+			"title": "Successful Login"}, "login-successful.html")
+	} else {
+		// If the username/password combination is invalid,
+		// show the error message on the login page
+		c.HTML(http.StatusBadRequest, "login.html", gin.H{
+			"ErrorTitle":   "Login Failed",
+			"ErrorMessage": "Invalid credentials provided"})
 	}
+}
+
+func generateSessionToken() string {
+	// We're using a random 16 character string as the session token
+	// This is NOT a secure way of generating session tokens
+	// DO NOT USE THIS IN PRODUCTION
+	return strconv.FormatInt(rand.Int63(), 16)
 }
